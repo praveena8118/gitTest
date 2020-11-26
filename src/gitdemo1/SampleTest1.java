8e99c3c5cce829eb8cec9620ba748c38ae9e399e
Rupendranathreddy <Rupendranathreddy@192.168.0.16>
2020-11-25 19:10:13 -0500

    =======This is my 1st commit=====

---
+++ src/gitdemo1/SampleTest1.java
@@ -0,0 +1,15 @@
+package gitdemo1;
+
+public class SampleTest1 {
+
+	public static void main(String[] args) {
+		String text="java";
+		if(text=="java") {
+		System.out.println("hello world of Git");
+	}
+	else {
+		System.out.println("hi");
+	}
+
+}
+}
